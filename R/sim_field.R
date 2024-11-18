9afa3e9b825dd644b75ed9bdd989ca9c5de3d1b2
Jakub Nowosad <tupiszakaczki@gmail.com>
2024-11-18 15:19:08 +0100

    documents and redo the sim_field code

--- R/sim_field.R
+++ R/sim_field.R
@@ -1,85 +1,74 @@
-#' Title
+#' Create a simulated field with covariates and outcome
 #'
-#' @param rast_grid
-#' @param range
-#' @param scenario
+#' @param rast_grid A raster object with the desired dimensions
+#' @param range The range of the semivariogram
+#' @param scenario The type of error to add to the outcome: 'autocor' or 'random'
+#' @param n_covariates The number of covariates to simulate
 #'
-#' @return
+#' @return A raster object with the simulated field and related covariates
 #' @export
 #'
 #' @examples
-sim_field = function(rast_grid, range, scenario) {
+#' rast_grid = terra::rast(ncols = 300, nrows = 100, xmin = 0, xmax = 300, ymin = 0, ymax = 100)
+#' sf1 = sim_field(rast_grid, range = 25, scenario = "autocor")
+#' sf2 = sim_field(rast_grid, range = 50, scenario = "autocor")
+#' sf3 = sim_field(rast_grid, range = 25, scenario = "random")
+#' terra::plot(sf1)
+#' terra::plot(sf2)
+#' terra::plot(sf3)
+sim_field = function(rast_grid, range, scenario, n_covariates = 6) {
 
-  # Print progress
-  # print(seed)
-  # set.seed(seed)
-
-  # Create an empty results object
-  res = data.frame()
-
-  # Create grids (raster and point format) and sampling and extrapolation area
-  # rast_grid = terra::rast(ncols = 300, nrows = 100,
-  #                         xmin = 0, xmax = 300,
-  #                         ymin = 0, ymax = 100)
-  point_grid = sf::st_as_sf(terra::as.points(rast_grid, values = FALSE, na.rm = FALSE))
+  rast_grid_coords = terra::crds(rast_grid, df = TRUE)
 
   # Simulate 6 covariates from a semivariogram and stack
   cov_mod = gstat::vgm(model = "Sph", psill = 1, range = range, nugget = 0)
   cov_mod = gstat::gstat(formula = z ~ 1, dummy = TRUE, beta = 0,
-                         model = cov_mod, nmax = 100)
-  cov_points = quiet(stats::predict(cov_mod, point_grid, nsim = 6))
-  cov_points = cbind(as.data.frame(sf::st_coordinates(point_grid)),
-                     sf::st_drop_geometry(cov_points))
-  cov_stack = terra::rast(cov_points)
-  names(cov_stack) = paste0("cov", 1:6)
-  rm("cov_mod")
+                         model = cov_mod, nmax = 100, locations = ~x + y)
+  cov_stack = quiet(stats::predict(cov_mod, rast_grid_coords, nsim = n_covariates))
+  cov_stack = terra::rast(cov_stack)
+  names(cov_stack) = paste0("cov", seq_len(n_covariates))
 
   # Generate outcome
-  out_rast = cov_stack$cov1 + cov_stack$cov2 * cov_stack$cov3 +
-    cov_stack$cov4 + cov_stack$cov5 * cov_stack$cov6
+  out_rast = generate_outcome(cov_stack)
 
-  # If error is autocorrelated
   if (scenario == "autocor") {
-    snoise_mod = gstat::vgm(model = "Sph", psill = 1, range = 25, nugget = 0)
-    snoise_mod = gstat::gstat(formula = z ~ 1, dummy = TRUE, beta = 0,
-                              model = snoise_mod, nmax = 100)
-    snoise_points = quiet(stats::predict(snoise_mod, point_grid, nsim = 1))
-    snoise_points = cbind(as.data.frame(sf::st_coordinates(point_grid)),
-                          sf::st_drop_geometry(snoise_points))
-    snoise = terra::rast(snoise_points)
-    names(snoise) = "error"
-    out_rast = out_rast + snoise
+    # If error is autocorrelated
+    autocor_mod = gstat::vgm(model = "Sph", psill = 1, range = 25, nugget = 0)
+    autocor_mod = gstat::gstat(formula = z ~ 1, dummy = TRUE, beta = 0,
+                              model = autocor_mod, nmax = 100, locations = ~x + y)
+
+    autocor_pred = quiet(stats::predict(autocor_mod, rast_grid_coords, nsim = 1))
+    autocor_pred = terra::rast(autocor_pred)
+    out_rast = out_rast + autocor_pred
     names(out_rast) = "outcome"
     all_stack = c(out_rast, cov_stack)
-  } else if (scenario == "random_noise"){
+
+  } else if (scenario == "random"){
     # otherwise add random noise
-    rnoise = terra::rast(ncols = 300, nrows = 100,
-                         xmin = 0, xmax = 300,
-                         ymin = 0, ymax = 100)
-    vals = stats::rnorm(300 * 100, sd = 1)
-    rnoise = terra::setValues(rnoise, vals)
+    vals = stats::rnorm(terra::ncell(rast_grid), sd = 1)
+    rnoise = terra::setValues(rast_grid, vals)
     out_rast = out_rast + rnoise
     names(out_rast) = "outcome"
     all_stack = c(out_rast, cov_stack)
   }
 
-  # Generate spatial proxies: coordinates
-  p_c = proxy_coordinates(rast_grid)
-
-  # Generate spatial proxies: EDF
-  p_edf = proxy_edf(rast_grid)
-
-  all_stack = c(all_stack, p_c, p_edf)
-
-  # Generate spatial proxies: RFsp
-  # all_points = sf::st_as_sf(terra::as.points(rast_grid, na.rm = FALSE))
-  # proxies_dist_RFsp = sf::st_distance(point_grid, all_points)
-  # proxies_RFsp = cbind(proxies_dist_RFsp, point_grid)
-  # proxies_RFsp = cbind(as.data.frame(sf::st_coordinates(proxies_RFsp)), sf::st_drop_geometry(proxies_RFsp))
-  # proxies_stack_RFsp = terra::rast(proxies_RFsp)
-  # names(proxies_stack_RFsp) = paste0("RFsp", 1:(ncol(proxies_RFsp)-2))
-  # all_stack = c(all_stack, proxies_stack_RFsp)
-
-  # Return the generated raster data
+  terra::crs(all_stack) = terra::crs(rast_grid)
   return(all_stack)
 }
+
+generate_outcome = function(cov_stack){
+  covs = names(cov_stack)
+  out_rast = 0
+
+  for (i in seq(1, length(covs), by = 3)) {
+    if (i + 2 <= length(covs)) {
+      out_rast = out_rast + cov_stack[[covs[i]]] + cov_stack[[covs[i + 1]]] * cov_stack[[covs[i + 2]]]
+    } else if (i + 1 <= length(covs)) {
+      out_rast = out_rast + cov_stack[[covs[i]]] + cov_stack[[covs[i + 1]]]
+    } else {
+      out_rast = out_rast + cov_stack[[covs[i]]]
+    }
+  }
+  return(out_rast)
+}
+
