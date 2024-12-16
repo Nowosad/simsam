ec88df6e1260890aef2c9532d760662b3ca2fe59
Jakub Nowosad <tupiszakaczki@gmail.com>
2024-12-16 14:11:06 +0100

    adds docs to sim_covariates and blend_rasters

--- R/sim_covariates.R
+++ R/sim_covariates.R
@@ -1,14 +1,28 @@
-#' Title
+#' Simulate a raster stack of covariates based on a variogram model
 #'
-#' @param rast_grid
-#' @param vgm
-#' @param n
-#' @param ...
+#' @param rast_grid A raster object with the desired dimensions
+#' @param vgm A variogram model object
+#' @param n The number of covariates to simulate
+#' @param ... Additional arguments to be passed to the `gstat::vgm()` function; `beta`, `nmax`, `indicators` (see Examples)
 #'
-#' @return
+#' @return A raster object with the simulated covariates
 #' @export
 #'
 #' @examples
+#' rast_grid = terra::rast(ncols = 300, nrows = 100,
+#'   xmin = 0, xmax = 300, ymin = 0, ymax = 100)
+#' sf1 = sim_covariates(rast_grid, range = 25,
+#'   vgm = gstat::vgm(model = "Exp", psill = 1, range = 10), n = 4)
+#' sf2 = sim_covariates(rast_grid, range = 50, n = 4)
+#' sf3 = sim_covariates(rast_grid, range = 25,
+#'   vgm = gstat::vgm(model = "Nug", psill = 2, range = 0), n = 4)
+#' sf4 = sim_covariates(rast_grid, range = 25,
+#'   vgm = gstat::vgm(model = "Exp", psill = 10, range = 100), n = 1,
+#'   beta = 30, indicators = TRUE)
+#' terra::plot(sf1)
+#' terra::plot(sf2)
+#' terra::plot(sf3)
+#' terra::plot(sf4)
 sim_covariates = function(rast_grid, vgm = NULL, n = 6, ...){
 
   additional_args = list(...)
@@ -28,8 +42,8 @@ sim_covariates = function(rast_grid, vgm = NULL, n = 6, ...){
 
   cov_mod = gstat::gstat(formula = z ~ 1, dummy = TRUE, beta = beta,
                          model = cov_mod, nmax = nmax, locations = ~x + y)
-  cov_stack = quiet(stats::predict(cov_mod, rast_grid_coords, nsim = n,
-                                   indicators = check_args("indicators", additional_args, ifnotfound = 0)))
+  suppressWarnings({cov_stack = quiet(stats::predict(cov_mod, rast_grid_coords, nsim = n,
+                                   indicators = check_args("indicators", additional_args, ifnotfound = 0)))})
   cov_stack = terra::rast(cov_stack)
   names(cov_stack) = paste0("cov", seq_len(n))
   return(cov_stack)
@@ -38,11 +52,3 @@ sim_covariates = function(rast_grid, vgm = NULL, n = 6, ...){
 check_args = function(arg_name, additional_args, ifnotfound){
   ifelse(arg_name %in% names(additional_args), get(arg_name, additional_args), ifnotfound)
 }
-
-# rast_grid = terra::rast(ncols = 300, nrows = 100, xmin = 0, xmax = 300, ymin = 0, ymax = 100)
-# sf1 = sim_covariates(rast_grid, range = 25)
-# sf2 = sim_covariates(rast_grid, vgm = gstat::vgm(model = "Exp", psill = 1, range = 25))
-# terra::plot(sf2)
-# sf3 = sim_covariates(rast_grid, vgm = gstat::vgm(nugget = 1))
-# sf3 = sim_covariates(rast_grid, vgm = gstat::vgm(model = "Nug", range = 0, psill = 10))
-# terra::plot(sf3)
