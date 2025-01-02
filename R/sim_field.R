781f19ba9fb141b3a7da2408d5d28b0f0dea315f
Jakub Nowosad <tupiszakaczki@gmail.com>
2025-01-02 10:49:21 +0100

    hides old sim_field

--- R/sim_field.R
+++ R/sim_field.R
@@ -6,7 +6,7 @@
 #' @param n_covariates The number of covariates to simulate
 #'
 #' @return A raster object with the simulated field and related covariates
-#' @export
+#' #@export
 #'
 #' @examples
 #' rast_grid = terra::rast(ncols = 300, nrows = 100, xmin = 0, xmax = 300, ymin = 0, ymax = 100)
@@ -16,62 +16,62 @@
 #' terra::plot(sf1)
 #' terra::plot(sf2)
 #' terra::plot(sf3)
-sim_field = function(rast_grid, range, scenario, n_covariates = 6) {
-
-  if (n_covariates == 0){
-    stop("'n_covariates' must be greater than 0")
-  }
-  rast_grid_coords = terra::crds(rast_grid, df = TRUE)
-
-  # Simulate covariates from a semivariogram and stack
-  cov_mod = gstat::vgm(model = "Sph", psill = 1, range = range, nugget = 0)
-  cov_mod = gstat::gstat(formula = z ~ 1, dummy = TRUE, beta = 0,
-                         model = cov_mod, nmax = 30, locations = ~x + y)
-  cov_stack = quiet(stats::predict(cov_mod, rast_grid_coords, nsim = n_covariates))
-  cov_stack = terra::rast(cov_stack)
-  names(cov_stack) = paste0("cov", seq_len(n_covariates))
-
-  # Generate outcome
-  out_rast = generate_outcome(cov_stack)
-
-  if (scenario == "autocor") {
-    # If error is autocorrelated
-    autocor_mod = gstat::vgm(model = "Sph", psill = 1, range = 25, nugget = 0)
-    autocor_mod = gstat::gstat(formula = z ~ 1, dummy = TRUE, beta = 0,
-                              model = autocor_mod, nmax = 30, locations = ~x + y)
-
-    autocor_pred = quiet(stats::predict(autocor_mod, rast_grid_coords, nsim = 1))
-    autocor_pred = terra::rast(autocor_pred)
-    out_rast = out_rast + autocor_pred
-    names(out_rast) = "outcome"
-    all_stack = c(out_rast, cov_stack)
-
-  } else if (scenario == "random"){
-    # otherwise add random noise
-    vals = stats::rnorm(terra::ncell(rast_grid), sd = 1)
-    rnoise = terra::setValues(rast_grid, vals)
-    out_rast = out_rast + rnoise
-    names(out_rast) = "outcome"
-    all_stack = c(out_rast, cov_stack)
-  }
-
-  terra::crs(all_stack) = terra::crs(rast_grid)
-  return(all_stack)
-}
-
-generate_outcome = function(cov_stack){
-  covs = names(cov_stack)
-  out_rast = 0
-
-  for (i in seq(1, length(covs), by = 3)) {
-    if (i + 2 <= length(covs)) {
-      out_rast = out_rast + cov_stack[[covs[i]]] + cov_stack[[covs[i + 1]]] * cov_stack[[covs[i + 2]]]
-    } else if (i + 1 <= length(covs)) {
-      out_rast = out_rast + cov_stack[[covs[i]]] + cov_stack[[covs[i + 1]]]
-    } else {
-      out_rast = out_rast + cov_stack[[covs[i]]]
-    }
-  }
-  return(out_rast)
-}
+# sim_field = function(rast_grid, range, scenario, n_covariates = 6) {
+#
+#   if (n_covariates == 0){
+#     stop("'n_covariates' must be greater than 0")
+#   }
+#   rast_grid_coords = terra::crds(rast_grid, df = TRUE)
+#
+#   # Simulate covariates from a semivariogram and stack
+#   cov_mod = gstat::vgm(model = "Sph", psill = 1, range = range, nugget = 0)
+#   cov_mod = gstat::gstat(formula = z ~ 1, dummy = TRUE, beta = 0,
+#                          model = cov_mod, nmax = 30, locations = ~x + y)
+#   cov_stack = quiet(stats::predict(cov_mod, rast_grid_coords, nsim = n_covariates))
+#   cov_stack = terra::rast(cov_stack)
+#   names(cov_stack) = paste0("cov", seq_len(n_covariates))
+#
+#   # Generate outcome
+#   out_rast = generate_outcome(cov_stack)
+#
+#   if (scenario == "autocor") {
+#     # If error is autocorrelated
+#     autocor_mod = gstat::vgm(model = "Sph", psill = 1, range = 25, nugget = 0)
+#     autocor_mod = gstat::gstat(formula = z ~ 1, dummy = TRUE, beta = 0,
+#                               model = autocor_mod, nmax = 30, locations = ~x + y)
+#
+#     autocor_pred = quiet(stats::predict(autocor_mod, rast_grid_coords, nsim = 1))
+#     autocor_pred = terra::rast(autocor_pred)
+#     out_rast = out_rast + autocor_pred
+#     names(out_rast) = "outcome"
+#     all_stack = c(out_rast, cov_stack)
+#
+#   } else if (scenario == "random"){
+#     # otherwise add random noise
+#     vals = stats::rnorm(terra::ncell(rast_grid), sd = 1)
+#     rnoise = terra::setValues(rast_grid, vals)
+#     out_rast = out_rast + rnoise
+#     names(out_rast) = "outcome"
+#     all_stack = c(out_rast, cov_stack)
+#   }
+#
+#   terra::crs(all_stack) = terra::crs(rast_grid)
+#   return(all_stack)
+# }
+#
+# generate_outcome = function(cov_stack){
+#   covs = names(cov_stack)
+#   out_rast = 0
+#
+#   for (i in seq(1, length(covs), by = 3)) {
+#     if (i + 2 <= length(covs)) {
+#       out_rast = out_rast + cov_stack[[covs[i]]] + cov_stack[[covs[i + 1]]] * cov_stack[[covs[i + 2]]]
+#     } else if (i + 1 <= length(covs)) {
+#       out_rast = out_rast + cov_stack[[covs[i]]] + cov_stack[[covs[i + 1]]]
+#     } else {
+#       out_rast = out_rast + cov_stack[[covs[i]]]
+#     }
+#   }
+#   return(out_rast)
+# }
 
--- README.md
+++ README.md
@@ -25,53 +25,8 @@ pak::pak("Nowosad/simsam")
 
 ## Examples
 
-Let’s start by loading the packages and creating a raster object that
-will serve as a grid template for the simulations.
-
-``` r
-library(simsam)
-library(terra)
-rast_grid = terra::rast(ncols = 200, nrows = 200, 
-                        xmin = 0, xmax = 200, ymin = 0, ymax = 200)
-```
-
-### Spatial data simulation
-
-The `sim_field()` function simulates a spatial field with covariates
-based on the provided arguments.
-
-``` r
-sim_rast = sim_field(rast_grid, 100, scenario = "autocor", n_covariates = 5)
-plot(sim_rast)
-```
-
-<img src="man/figures/README-unnamed-chunk-2-1.png" width="100%" />
-
-### Creating spatial proxies
-
-The `make_proxy()` function creates various spatial proxies (e.g.,
-coordinates, Euclidean Distance Fields (EDF), and Oblique Geographic
-Coordinates (OGC)) based on the provided input raster.
-
-``` r
-proxy_edf = make_proxy(sim_rast, "edf")
-plot(proxy_edf)
-```
-
-<img src="man/figures/README-unnamed-chunk-3-1.png" width="100%" />
-
-### Sampling spatial data
-
-The `sam_field()` function samples a spatial field based on the provided
-arguments.
-
-``` r
-set.seed(2024-12-04)
-sample_c1 = sam_field(rast_grid, 100, "clustered", nclusters = 8, value = 5)
-plot(sample_c1)
-```
-
-<img src="man/figures/README-unnamed-chunk-4-1.png" width="100%" />
+Examples of the package usage can be found in the
+[vignettes](https://nowosad.github.io/simsam/articles/).
 
 ## Contribution
 
