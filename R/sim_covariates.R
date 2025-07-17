29a917d4407e324af0aabc699d5984952fcc1771
Jakub Nowosad <tupiszakaczki@gmail.com>
2025-07-17 14:54:19 +0200

    sets crs to local

--- R/sim_covariates.R
+++ R/sim_covariates.R
@@ -44,7 +44,7 @@ sim_covariates = function(rast_grid, vgm = NULL, n = 6, ...){
                          model = cov_mod, nmax = nmax, locations = ~x + y)
   suppressWarnings({cov_stack = quiet(stats::predict(cov_mod, rast_grid_coords, nsim = n,
                                    indicators = check_args("indicators", additional_args, ifnotfound = 0)))})
-  cov_stack = terra::rast(cov_stack)
+  cov_stack = terra::rast(cov_stack, crs = "local")
   names(cov_stack) = paste0("cov", seq_len(n))
   return(cov_stack)
 }
