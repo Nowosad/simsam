f65d904b0c1421b52d9e1c2bb1bf507f70025913
Jakub Nowosad <tupiszakaczki@gmail.com>
2026-03-27 14:30:23 +0100

    updates blend_rasters to new syntax -- also it uses right side of the formula only

--- R/blend_rasters.R
+++ R/blend_rasters.R
@@ -1,7 +1,7 @@
 #' Blend rasters based on a provided formula
 #'
-#' @param r A `SpatRaster` object with one or more layers
-#' @param formula A formula that specifies how to combine the rasters
+#' @param x A `SpatRaster` object with one or more layers
+#' @param formula A formula specifying how to combine the rasters
 #' @param ... Additional `SpatRaster` objects to blend
 #'
 #' @return A `SpatRaster` object with the combined layers
@@ -10,22 +10,61 @@
 #' @examples
 #' rast_grid = terra::rast(ncols = 300, nrows = 100,
 #'   xmin = 0, xmax = 300, ymin = 0, ymax = 100)
-#' sf1 = sim_covariates(rast_grid, range = 25,
-#'   vgm = gstat::vgm(model = "Exp", psill = 1, range = 10), n = 4)
-#' g1 = blend_rasters(sf1, y ~ cov1 + cov4)
-#' g2 = blend_rasters(g1, y ~ outcome^2 + (cov2 + 2), sf1)
+#'
+#' sf1 = sim_covariates(
+#'   rast_grid,
+#'   n = 4,
+#'   method = simulate_gaussian(range = 25)
+#' )
+#'
+#' g1 = blend_rasters(sf1, ~ cov1 + cov4)
+#' g2 = blend_rasters(g1, ~ outcome^2 + (cov2 + 2), sf1)
+#'
 #' terra::plot(g1)
 #' terra::plot(g2)
-blend_rasters = function(r, formula, ...){
-  r = c(r, ...)
+blend_rasters = function(x, formula, ...) {
+
+  rasters = list(x, ...)
+  rasters = do.call(c, rasters)
+
+  if (!inherits(rasters, "SpatRaster")) {
+    stop("All inputs must be SpatRaster objects")
+  }
+  if (is.null(names(rasters))) {
+    stop("All raster layers must have names")
+  }
+  if (anyDuplicated(names(rasters))) {
+    stop("Raster layer names must be unique")
+  }
+
   f = as_function(formula)
-  output_raster = terra::lapp(r, f, usenames = TRUE)
-  names(output_raster) = "outcome"
-  return(output_raster)
+
+  output = terra::lapp(rasters, f, usenames = TRUE)
+
+  if (terra::nlyr(output) == 1) {
+    names(output) = "outcome"
+  }
+
+  return(output)
 }
-# https://stackoverflow.com/a/36680963 by https://stackoverflow.com/users/601303/thilo
+
+
+# Convert formula to function (but left-hand side is ignored)
 as_function = function(formula) {
-    cmd = utils::tail(as.character(formula), 1)
-    exp = parse(text = cmd)
-    function(...) eval(exp, list(...))
+
+  if (!inherits(formula, "formula")) {
+    stop("`formula` must be a formula")
+  }
+
+  env = environment(formula)
+
+  rhs = if (length(formula) == 3) {
+    formula[[3]]
+  } else {
+    formula[[2]]
+  }
+
+  function(...) {
+    eval(rhs, envir = list(...), enclos = env)
+  }
 }
