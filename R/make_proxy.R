03e17c9d303df5269186bf15e342f4a33ddd0d60
Jakub Nowosad <tupiszakaczki@gmail.com>
2026-03-27 14:42:43 +0100

    updates syntax of make_proxy; fixes an issue wit ogc

--- R/make_proxy.R
+++ R/make_proxy.R
@@ -1,114 +1,222 @@
 #' Generate spatial proxies
 #'
 #' The `make_proxy()` function generates spatial proxies for a given raster object.
-#' It generates three types of proxies: coordinates, Euclidean Distance Fields (EDF), and Oblique Geographic Coordinates (OGC).
-#' Additionally, the `add_proxy()` function can be used to add a proxy to an existing raster object.
+#' It generates spatial proxies such as:
+#' - coordinates
+#' - Euclidean Distance Fields (EDF)
+#' - Oblique Geographic Coordinates (OGC)
 #'
-#' @param rast_grid A raster object with the desired dimensions
-#' @param type The type of proxy to generate. Options are coordinates ("coordinates"), Euclidean Distance Fields ("edf"), and Oblique Geographic Coordinates ("ogc")
-#' @param opts A list of options to pass to the proxy generation function. Only used when `type = "edf"` or `type = "ogc"`.
-#' When `type = "edf"`, the "`coords`" option is and sf object a matrix with two columns (x, y) representing the coordinates to use when generating Euclidean Distance Fields. When `type = "ogc"`, the "`n`" option is an integer representing the number of angles to use when generating Oblique Geographic Coordinates.
+#' @param rast_grid A `SpatRaster` object
+#' @param method A proxy generation method created by `proxy_*()` functions
 #'
 #' @references Behrens, T., Schmidt, K., Viscarra Rossel, R. A., Gries, P., Scholten, T., & MacMillan, R. A. (2018). Spatial modelling with Euclidean distance fields and machine learning. European journal of soil science, 69(5), 757-770.
 #' @references Møller, A. B., Beucher, A. M., Pouladi, N., & Greve, M. H. (2020). Oblique geographic coordinates as covariates for digital soil mapping. Soil, 6(2), 269-289.
 #'
-#' @return A SpatRaster object with the generated spatial proxies
+#' @return A `SpatRaster` object
 #' @export
 #'
 #' @examples
-#' rast_grid = terra::rast(ncols = 300, nrows = 100, xmin = 0, xmax = 300, ymin = 0, ymax = 100)
-#' proxy_coords = make_proxy(rast_grid, "coordinates")
-#' proxy_edf = make_proxy(rast_grid, "edf")
-#' proxy_ogc = make_proxy(rast_grid, "ogc", opts = list(n = 5))
+#' rast_grid = terra::rast(
+#'   ncols = 300, nrows = 100,
+#'   xmin = 0, xmax = 300,
+#'   ymin = 0, ymax = 100
+#' )
+#'
+#' proxy_coords = make_proxy(rast_grid, proxy_coordinates())
+#' proxy_edf = make_proxy(rast_grid, proxy_edf())
+#' proxy_ogc = make_proxy(rast_grid, proxy_ogc(n = 5))
+#'
 #' terra::plot(proxy_coords)
 #' terra::plot(proxy_edf)
 #' terra::plot(proxy_ogc)
-make_proxy = function(rast_grid, type, opts = NULL){
-  if(type == "coordinates"){
-    return(proxy_coordinates(rast_grid))
-  } else if(type == "edf"){
-    return(proxy_edf(rast_grid, opts$coords))
-  } else if(type == "ogc"){
-    return(proxy_ogc(rast_grid, opts$n))
+make_proxy = function(
+  rast_grid,
+  method = proxy_coordinates()
+) {
+
+  if (!inherits(rast_grid, "SpatRaster")) {
+    stop("`rast_grid` must be a SpatRaster")
   }
+  if (!is.function(method)) {
+    stop("`method` must be a function")
+  }
+
+  proxy = method(rast_grid)
+
+  if (!inherits(proxy, "SpatRaster")) {
+    stop("Proxy method must return a SpatRaster")
+  }
+
+  proxy
 }
+
+#' Add spatial proxy to raster
+#'
+#' @param rast_grid A `SpatRaster`
+#' @param method A proxy method created by `proxy_*()` functions
+#'
+#' @return `SpatRaster`
 #' @export
 #' @rdname make_proxy
-add_proxy = function(rast_grid, type, opts = NULL){
-  c(rast_grid, make_proxy(rast_grid, type, opts))
+add_proxy = function(
+  rast_grid,
+  method = proxy_coordinates()
+) {
+  c(rast_grid, make_proxy(rast_grid, method))
 }
 
-proxy_coordinates = function(rast_grid){
-  rast_x = terra::init(rast_grid, "x")
-  rast_y = terra::init(rast_grid, "y")
-  proxy_coords_stack = c(rast_x, rast_y)
-  names(proxy_coords_stack) = c("X", "Y")
-  return(proxy_coords_stack)
+#' Coordinate proxy factory
+#'
+#' Returns a function that generates coordinate layers (X, Y) for a raster.
+#'
+#' @return A function that accepts `rast_grid` and returns a `SpatRaster`
+#' @export
+#'
+#' @examples
+#' rast_grid = terra::rast(
+#'   ncols = 300, nrows = 100,
+#'   xmin = 0, xmax = 300,
+#'   ymin = 0, ymax = 100
+#' )
+#'
+#' make_proxy(rast_grid, proxy_coordinates())
+proxy_coordinates = function() {
+
+  function(rast_grid) {
+    rast_x = terra::init(rast_grid, "x")
+    rast_y = terra::init(rast_grid, "y")
+    proxy_coords_stack = c(rast_x, rast_y)
+    names(proxy_coords_stack) = c("X", "Y")
+    return(proxy_coords_stack)
+  }
 }
 
-proxy_edf = function(rast_grid, coords = NULL) {
+#' Euclidean Distance Field proxy factory
+#'
+#' Returns a function that generates Euclidean Distance Fields from specified
+#' locations (default: four corners and center of the raster extent).
+#'
+#' @param coords Optional coordinates. Can be a matrix, data frame, `sf`, or
+#'   `SpatVector` with x and y columns. If `NULL`, uses default locations: four corners and center of the raster extent.
+#'
+#' @return A function that accepts `rast_grid` and returns a `SpatRaster`
+#' @export
+#'
+#' @examples
+#' rast_grid = terra::rast(
+#'   ncols = 300, nrows = 100,
+#'   xmin = 0, xmax = 300,
+#'   ymin = 0, ymax = 100
+#' )
+#'
+#' # Default: four corners and center
+#' make_proxy(rast_grid, proxy_edf())
+#'
+#' # Custom coordinates
+#' custom_coords = matrix(c(50, 50, 150, 150), ncol = 2, byrow = TRUE)
+#' rownames(custom_coords) = c("point1", "point2")
+#' make_proxy(rast_grid, proxy_edf(coords = custom_coords))
+proxy_edf = function(coords = NULL) {
 
-  # If no custom coordinates are provided, use default: four corners and center.
-  if (is.null(coords)) {
-    rast_extent = as.vector(terra::ext(rast_grid))
-    xmin = rast_extent[1]; xmax = rast_extent[2]
-    ymin = rast_extent[3]; ymax = rast_extent[4]
-    coords = rbind(
-      EDF1 = c(xmin, ymax),
-      EDF2 = c(xmax, ymax),
-      EDF3 = c(xmin, ymin),
-      EDF4 = c(xmax, ymin),
-      EDF5 = c((xmin + xmax) / 2, (ymin + ymax) / 2)
+  function(rast_grid) {
+
+    # If no custom coordinates are provided, use default: four corners and center.
+    if (is.null(coords)) {
+      rast_extent = as.vector(terra::ext(rast_grid))
+      xmin = rast_extent[1]; xmax = rast_extent[2]
+      ymin = rast_extent[3]; ymax = rast_extent[4]
+      coords_local = rbind(
+        EDF1 = c(xmin, ymax),
+        EDF2 = c(xmax, ymax),
+        EDF3 = c(xmin, ymin),
+        EDF4 = c(xmax, ymin),
+        EDF5 = c((xmin + xmax) / 2, (ymin + ymax) / 2)
+      )
+    } else {
+      # Ensure coords is a matrix with two columns.
+      coords_local = coords
+      if (inherits(coords_local, "sf")) {
+        coords_local = terra::vect(coords_local)
+      }
+      if (inherits(coords_local, "SpatVector")) {
+        coords_local = terra::crds(coords_local)
+      }
+      coords_local = as.matrix(coords_local)
+      if (ncol(coords_local) != 2) {
+        stop("coords must be a matrix (or convertible to) with two columns (x, y).")
+      }
+      if (is.null(rownames(coords_local))) {
+        rownames(coords_local) = paste0("EDF", seq_len(nrow(coords_local)))
+      }
+    }
+
+    # Use appropriate coordinate flag based on CRS (lonlat or not)
+    ll_crs = is_lonlat(rast_grid)
+
+    rast_grid_coords = terra::crds(rast_grid)
+    # Compute distances for each provided point; resulting in a data.frame with one column per point.
+    proxy_vals_list = lapply(1:nrow(coords_local), function(i) {
+      terra::distance(rast_grid_coords, coords_local[i, , drop = FALSE], lonlat = ll_crs)
+    })
+    proxy_edf_stack_vals = as.data.frame(do.call(cbind, proxy_vals_list))
+    proxy_edf_stack = terra::rast(rast_grid, nl = ncol(proxy_edf_stack_vals))
+    proxy_edf_stack = terra::setValues(proxy_edf_stack, proxy_edf_stack_vals)
+    names(proxy_edf_stack) = rownames(coords_local)
+
+    return(proxy_edf_stack)
+  }
+}
+
+#' Oblique Geographic Coordinates proxy factory
+#'
+#' Returns a function that generates Oblique Geographic Coordinates (OGC) as covariates.
+#'
+#' @param n Number of angles (default: 5)
+#'
+#' @return A function that accepts `rast_grid` and returns a `SpatRaster`
+#' @export
+#'
+#' @references
+#' Møller, A. B., Beuchert, A. M., Pouladi, N., & Greve, M. H. (2020).
+#' Oblique geographic coordinates as covariates for digital soil mapping.
+#' Soil, 6(2), 269-289.
+#'
+#' @examples
+#' rast_grid = terra::rast(
+#'   ncols = 300, nrows = 100,
+#'   xmin = 0, xmax = 300,
+#'   ymin = 0, ymax = 100
+#' )
+#'
+#' make_proxy(rast_grid, proxy_ogc(n = 5))
+proxy_ogc = function(n = 5) {
+
+  function(rast_grid) {
+    proxy_ogc_stack = vector("list", n)
+    rast_x = terra::init(rast_grid, "x")
+    rast_y = terra::init(rast_grid, "y")
+
+    theta = terra::app(
+      c(rast_x, rast_y),
+      function(xy) atan2(xy[2], xy[1])
     )
-  } else {
-    # Ensure coords is a matrix with two columns.
-    if (inherits(coords, "sf")) {
-      coords = terra::vect(coords)
+    r = sqrt(rast_x^2 + rast_y^2)
+
+    angles = seq(0, 180, length.out = n + 1)
+    angles = angles[-length(angles)] * pi / 180
+
+    out = vector("list", n)
+
+    for (i in seq_along(angles)) {
+      out[[i]] = cos(angles[i] - theta) * r
     }
-    if (inherits(coords, "SpatVector")) {
-      coords = terra::crds(coords)
-    }
-    coords = as.matrix(coords)
-    if (ncol(coords) != 2) {
-      stop("coords must be a matrix (or convertible to) with two columns (x, y).")
-    }
-    if (is.null(rownames(coords))) {
-      rownames(coords) = paste0("EDF", seq_len(nrow(coords)))
-    }
+
+    out = terra::rast(out)
+
+    names(out) = paste0("OGC_", round(angles * 180 / pi, 1))
+
+    out
   }
-
-  # Use appropriate coordinate flag based on CRS (lonlat or not)
-  ll_crs = is_lonlat(rast_grid)
-
-  rast_grid_coords = terra::crds(rast_grid)
-  # Compute distances for each provided point; resulting in a data.frame with one column per point.
-  proxy_vals_list = lapply(1:nrow(coords), function(i) {
-    terra::distance(rast_grid_coords, coords[i, , drop = FALSE], lonlat = ll_crs)
-  })
-  proxy_edf_stack_vals = as.data.frame(do.call(cbind, proxy_vals_list))
-  proxy_edf_stack = terra::rast(rast_grid, nl = ncol(proxy_edf_stack_vals))
-  proxy_edf_stack = terra::setValues(proxy_edf_stack, proxy_edf_stack_vals)
-  names(proxy_edf_stack) = rownames(coords)
-
-  return(proxy_edf_stack)
-}
-
-proxy_ogc = function(rast_grid, n){
-  proxy_ogc_stack = vector("list", n)
-  rast_x = terra::init(rast_grid, "x")
-  rast_y = terra::init(rast_grid, "y")
-
-  angles = seq(0, 180, length.out = n + 1) * pi / 180
-  angles = angles[1:n]
-
-  for (i in seq_along(angles)){
-    proxy_ogc_stack[[i]] = cos(angles[i] - atan(rast_y / rast_x)) * sqrt(rast_x^2 + rast_y^2)
-  }
-
-  proxy_ogc_stack = terra::rast(proxy_ogc_stack)
-  # proxy_ogc_stack = terra::mask(proxy_ogc_stack, rast_grid) # to mask or not to mask?
-  names(proxy_ogc_stack) = paste0("Angle_", angles * 180 / pi)
-  return(proxy_ogc_stack)
 }
 
 # proxy_ = function(rast_grid){
@@ -129,4 +237,3 @@ proxy_ogc = function(rast_grid, n){
 #   rast_grid_vals = terra::as.points(rast_grid, na.rm = FALSE)
 #   d0 = terra::distance(rast_grid_vals, rast_grid_coords)
 # }
-
--- inst/tinytest/test_make_proxy.R
+++ inst/tinytest/test_make_proxy.R
@@ -3,15 +3,27 @@ library(terra)
 cov_grid = rast(nrows = 10, ncols = 10)
 values(cov_grid) = runif(ncell(cov_grid))
 
-proxy_raster = make_proxy(cov_grid, type = "coordinates")
+# Test coordinates
+proxy_raster = make_proxy(cov_grid, method = proxy_coordinates())
 expect_true(inherits(proxy_raster, "SpatRaster"), info = "make_proxy should return a SpatRaster object")
+expect_equal(nlyr(proxy_raster), 2, info = "coordinates proxy should return 2 layers")
 
+# Test EDF
 cov_grid2 = rast(nrows = 10, ncols = 10, crs = "local")
 values(cov_grid2) = runif(ncell(cov_grid2))
 
-proxy_raster2 = make_proxy(cov_grid2, type = "edf")
-expect_equal(nlyr(proxy_raster2), 5, info = "make_proxy should return the correct number of layers")
+proxy_raster2 = make_proxy(cov_grid2, method = proxy_edf())
+expect_equal(nlyr(proxy_raster2), 5, info = "EDF proxy should return 5 layers (default)")
 
-proxy_raster3 = make_proxy(cov_grid, type = "ogc", opts = list(n = 3))
-expect_equal(nlyr(proxy_raster3), 3, info = "make_proxy should return the correct number of layers")
+# Test OGC
+proxy_raster3 = make_proxy(cov_grid, method = proxy_ogc(n = 3))
+expect_equal(nlyr(proxy_raster3), 3, info = "OGC proxy should return the specified number of layers")
 
+# Test add_proxy
+proxy_raster4 = add_proxy(cov_grid, method = proxy_coordinates())
+expect_equal(nlyr(proxy_raster4), 3, info = "add_proxy should add 2 layers to existing raster")
+
+# Test reusable method
+coords_method = proxy_coordinates()
+proxy_raster5 = make_proxy(cov_grid, method = coords_method)
+expect_true(inherits(proxy_raster5, "SpatRaster"), info = "Reusable method should work")
